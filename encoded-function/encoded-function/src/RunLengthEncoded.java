a18024e7771b5abc4471ae9824c1bcab874d5e85
georgi-mr <160781027+georgi-mr@users.noreply.github.com>
2025-12-11 20:56:49 +0200

    update RunLengthEncoded.java to support char array

--- encoded-function/encoded-function/src/RunLengthEncoded.java
+++ encoded-function/encoded-function/src/RunLengthEncoded.java
@@ -1,51 +1,38 @@
-public class RunLengthEncoded {
 
-        public static void main(String[] args) throws Exception {
+    public class RunLengthEncoded {
 
-            String rez1 =encode("aaaabbbccc");
+        public static void main(String[] args) {
+            String rez1 = encode("aaaabbbccc".toCharArray());
             System.out.println(rez1);
-            String rez2 =encode("abbbcdddd");
+
+            String rez2 = encode("abbbcdddd".toCharArray());
             System.out.println(rez2);
-            //Testing null or empty string
-            //encode("");
-            //encode(" ");
-            String rezNum=encode("3333344442222");
+
+            String rezNum = encode("3333344442222".toCharArray());
             System.out.println(rezNum);
         }
 
-        public static String encode(String str) throws Exception {
+        public static String encode(char[] chars) {
 
-            //Validate string
-            if(str.isBlank()){
-
-                throw new Exception(" =( --> The string cannot be null or empty");
-
-            } else{
-
-                if(str.chars().allMatch(Character::isDigit)){
-
-                    System.out.println("We have numbers in our string! You should pay more attention =] -->");
-
-                } else{
-
-                    System.out.println("Congratulations! We have a clear string <3 <3 -->");
-                }
-
-
-            // Logic->process string
-               StringBuilder builder=new StringBuilder();
-                int count=1;
-                for(int i=0;i<str.length()-1;i++){
-                    if(str.charAt(i)==str.charAt(i+1)){
-                       count++;
-                    } else{
-                        builder.append(str.charAt(i)).append(count);
-                        count=1;
-                    }
-
-                }
-                builder.append(str.charAt(str.length()-1)).append(count);
-                return builder.toString();
+            if (chars == null || chars.length == 0) {
+                throw new IllegalArgumentException("The char array cannot be null or empty");
             }
+
+            StringBuilder builder = new StringBuilder();
+            int count = 1;
+
+
+            for (int i = 1; i < chars.length; i++) {
+                if (chars[i] == chars[i - 1]) {
+                    count++;
+                } else {
+                    builder.append(chars[i - 1]).append(count);
+                    count = 1;
+                }
+            }
+
+            builder.append(chars[chars.length - 1]).append(count);
+
+            return builder.toString();
         }
-}
+    }
