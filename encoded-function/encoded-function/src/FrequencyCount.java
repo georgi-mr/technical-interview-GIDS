4402fd2c1bbfb56bc39562cdb1b83b15769ecc3f
georgi-mr <160781027+georgi-mr@users.noreply.github.com>
2025-12-11 21:02:14 +0200

    update FrequencyCount.java to support char array

--- encoded-function/encoded-function/src/FrequencyCount.java
+++ encoded-function/encoded-function/src/FrequencyCount.java
@@ -2,51 +2,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FrequencyCount {
-    public static void main(String[] args) throws Exception {
 
-        String rez1 =encode("aaaabbbccc");
+    public static void main(String[] args) {
+        String rez1 = encode("aaaabbbccc".toCharArray());
         System.out.println(rez1);
-        String rez2 =encode("abbbcdddd");
+
+        String rez2 = encode("abbbcdddd".toCharArray());
         System.out.println(rez2);
-        //Testing null or empty string
-        //encode("");
-        //encode(" ");
-        String rezNum=encode("3333344442222");
+
+        String rezNum = encode("3333344442222".toCharArray());
         System.out.println(rezNum);
     }
 
-    public static String encode(String str) throws Exception {
-
-        //Validate string
-        if(str.isBlank()){
-
-            throw new Exception(" =( -> The string cannot be null or empty");
-
-        } else{
-
-            if(str.chars().allMatch(Character::isDigit)){
-
-                System.out.println("We have numbers in our string! You should pay more attention =] -->");
-
-            } else{
-
-                System.out.println("Congratulations! We have a clear string <3 <3 -->");
+    public static String encode(char[] chars) {
+        if (chars == null || chars.length == 0) {
+            throw new IllegalArgumentException("The char array cannot be null or empty");
+        } else {
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : chars) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
             }
 
-            // Logic->process string
-            Map<Character,Integer>map=new HashMap<>();
-            for(int i=0;i<str.length();i++){
-                Character c=str.charAt(i);
-                map.put(c,map.getOrDefault(c,0)+1);
-            }
-            StringBuilder stringBuilder=new StringBuilder();
-
-            for(Map.Entry<Character,Integer> entry:map.entrySet()){
-                stringBuilder.append(entry.getKey()).append(entry.getValue());
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                sb.append(entry.getKey()).append(entry.getValue());
             }
 
-            return stringBuilder.toString();
+            return sb.toString();
         }
     }
-
 }
